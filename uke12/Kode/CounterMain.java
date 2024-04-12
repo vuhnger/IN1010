b644c816b6db44bf05501062fc3e59d415361c6e
vuhger <victor.uhnger@gmail.com>
2024-04-12 11:45:03 +0200

    refaktorering

--- uke12/Kode/CounterMain.java
+++ uke12/Kode/CounterMain.java
@@ -1,3 +1,17 @@
+class Counter implements Runnable{
+    
+    private final int MAX_COUNT = 10_000; 
+    private int count = 0;
+
+    public void run(){
+        System.out.println("Starter, antall er: " + count);
+        for (int i = 0; i < MAX_COUNT; i++){
+            count++;
+        }
+        System.out.println("Ferdig, antall er: " + count);
+    }
+}
+
 public class CounterMain {
     public static void main(String[] args) {
         Runnable task = new Counter();
