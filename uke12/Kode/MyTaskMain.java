c0da4cafe6d6227e8b2da1b0c4f2f8ef9a433405
vuhger <victor.uhnger@gmail.com>
2024-04-12 11:30:45 +0200

    refaktorere litt

--- uke12/Kode/MyTaskMain.java
+++ uke12/Kode/MyTaskMain.java
@@ -1,3 +1,14 @@
+class MyTask implements Runnable{
+
+    int num;
+    MyTask(int num){ this.num = num;}
+
+    public void run(){
+        for (int i = num; i >= 1; i--){
+            System.out.println("Teller ned ... " + i);
+        }
+    }
+}
 
 public class MyTaskMain {
 
